78026453ec96c4c9746883cad6f60eeb9ccf0619
Honkato <meira.gmrm@hotmail.com>
2024-10-23 20:51:41 -0300

    qrcode adicionado

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,7 +1,20 @@
+import dragon_dance from '../assets/dancing-toothless-v0-q18gv0dfd4ac1.gif'
+import samsung from '../assets/samsung-celulares.png'
+import { useRef, useState } from 'react';
 function Logo({logoName}) {
+    const [image, setImage] = useState(samsung)
+    // const imageWithFallback = ({ src }) => {
+    //     const imgRef = useRef();
+    //     const onImageError = () => imgRef.current.src=dragon_dance;
+    //     return (
+    //         <img ref={imgRef} src={src} onError={onImageError} />
+    //     )
+    // }
+    const erro = () =>{setImage(dragon_dance)}
     return ( 
-        <div className="rounded-full bg-slate-600 h-24 w-24 flex justify-center object-center items-center flex-wrap overflow-hidden">
-            {logoName}
+        <div className="rounded-full bg-[#a58c55] h-24 w-24 flex justify-center object-center items-center flex-wrap overflow-hidden">
+            <img className='' src={image} onError={()=>erro()} alt="dragon_dance"/>
+            {/* {imageWithFallback('.')} */}
         </div>
      );
 }
