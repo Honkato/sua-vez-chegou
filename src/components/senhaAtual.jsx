99a437e6c0545d4b5aa422b0602ab6d6fae19e64
Honkato <meira.gmrm@hotmail.com>
2024-10-23 19:50:58 -0300

    qrcode adicionado

--- src/components/senhaAtual.jsx
+++ src/components/senhaAtual.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
-function SenhaAtual({usuario}) {
+function SenhaAtual({usuario, bgColor, fontColor}) {
     const [senha, setSenha] = useState(0)
     
     return ( 
-        <div className="h-16 w-16 p-5 rounded-lg bg-white text-orange-400 flex justify-center items-center font-semibold text-3xl">
+        <div 
+        style={{backgroundColor:bgColor??'#000', color:fontColor??'#fff'}}
+        className="h-16 w-16 p-5 rounded-lg flex justify-center items-center font-semibold text-3xl">
             {senha}
         </div>
      );
--- src/pages/qrCodePage.jsx
+++ src/pages/qrCodePage.jsx
@@ -1,12 +1,20 @@
 import { Link, useNavigate, useNavigation, useParams } from "react-router-dom";
 import Logo from "../components/logo";
 import SenhaAtual from "../components/senhaAtual";
-
+// import QRCode from '../service/qrcode'
 function QRCodePage() {
 
     const navigate = useNavigate()
     const params = useParams()
     const empresa = params.empresa
+    // var qrcode = new QRCode("test", {
+    //     text: "http://jindo.dev.naver.com/collie",
+    //     width: 128,
+    //     height: 128,
+    //     colorDark : "#000000",
+    //     colorLight : "#ffffff",
+    //     correctLevel : QRCode.CorrectLevel.H
+    // });
     return ( 
     <div className="w-full h-screen bg-orange-950 flex justify-center items-center flex-col text-wrap text-center">
         <Logo logoName={empresa}/>
